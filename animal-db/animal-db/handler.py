3ee9217036b8e7e6f0f5497851bbb6f2003c75b9
Ubuntu <ubuntu@ip-172-31-45-236.ec2.internal>
2020-07-28 12:54:42 +0000

    Added function folder

--- animal-db/animal-db/handler.py
+++ animal-db/animal-db/handler.py
@@ -2,35 +2,19 @@ import csv
 from io import StringIO
 #import matplotlib.pyplot as plt
 import pandas as pd
-import requests
+#import requests
 #import seaborn as sns
-import os
+#import os
 
-def get_data(animal_taxon, directory_path):
-    #create directory to save files
-    directory = str(animal_taxon) + '_data'
-    path = os.path.join(directory_path, directory)
-    os.mkdir(path)
-
-
-    headers = {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    }
-
-    data = {
-    'result': 'read_run',
-    'query': 'tax_tree('+ str(animal_taxon)+')',
-    'fields': 'scientific_name,tax_id,study_accession,study_alias,study_title,sample_accession,sample_alias,accession,secondary_sample_accession,run_accession,base_count,read_count,description,experiment_title,instrument_platform,instrument_model,library_source,library_selection,library_layout,library_name,library_strategy,sequencing_method,sample_description,sample_title,sex,cell_type,tissue_type,first_public,last_updated,fastq_galaxy,fastq_ftp,sra_ftp,sra_galaxy,submitted_ftp,submitted_galaxy,project_name',
-    'format': 'tsv'
-    }
-
-    response = requests.post('https://www.ebi.ac.uk/ena/portal/api/search', headers=headers, data=data)
-
-    data = StringIO(response.text)
-    df = pd.read_csv(data, sep = '\t')
+def get_data(db_url):
+    df = pd.read_csv(db_url, sep = ',')
 
   #return some information about the libraries
+    print("Source database information:\n")
+    print(df.info())
+    print("Library source: \n")
     print(pd.value_counts(df['library_source']))
+    print("\nLibrary strategy:\n")
     print(pd.value_counts(df['library_strategy']))
 
 
@@ -50,10 +34,11 @@ def get_data(animal_taxon, directory_path):
   #plt.show()
 
 
-
-    df.to_csv(os.path.join(path, str(animal_taxon)+'_database.csv'))
-    multiple_runs.to_csv(os.path.join(path, str(animal_taxon)+'_multiple_runs.csv'))
-    multiple_samples.to_csv(os.path.join(path, str(animal_taxon)+'_multiple_samples.csv'))
+    print("\nMultiple run accessions:\n") 
+    print(multiple_runs)
+    print("\nMultiple sample accessions per study:\n")
+    print(multiple_samples)
+    
     return()
   #plot the library information
 '''
@@ -61,7 +46,6 @@ def get_data(animal_taxon, directory_path):
   plt.xticks(rotation=90)
   plt.tight_layout()
   plt.savefig(os.path.join(path, str(animal_taxon)+'_library_strategy.png'))
-
   sns.countplot(x = df['library_source'], palette= 'muted')
   plt.xticks(rotation=90)
   plt.tight_layout()
@@ -72,8 +56,8 @@ def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
-    """
+    
     animal_and_directory = req
     animal = req.split(",")[0]
-    directory = req.split(",")[1]
-    return get_data(animal, directory)
+    directory = req.split(",")[1]"""
+    return get_data(req)
